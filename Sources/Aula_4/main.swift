3edaa9bcc2978209c5bd0450f5d49d97967a7cd4
Theo Espósito <thesposito.estudos@gmail.com>
2025-09-04 18:54:47 +0000

    Heranca e Polimorfismo

--- Sources/Aula_4/main.swift
+++ Sources/Aula_4/main.swift
@@ -1,3 +1,9 @@
-var carro: Carro = CarroEsportivo(marca:"Ford", modelo:"Mustang", senha:"123", velocidadeMaxima: 200.3)
+let car: Carro2 = Carro2(marca: "Fiat", ano: 1996, numeroDeRodas: 40)
+// car.apresentar()
 
-print(carro.getModelo())
+var moto = Moto(marca: "Honda", ano: 2015, cilindradas: 5)
+var array: [Veiculo] = [moto, car, Moto(marca: "Yamaha", ano: 2025, cilindradas: 600)] 
+
+for item in array {
+    print(item.detalhes())
+}
